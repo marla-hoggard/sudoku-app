3c454e5d2fca75cd0f5b40dbad112ae341065ff4
Marla Glasser <mbglasser@gmail.com>
2018-05-30 16:43:07 -0400

    Add redux devtools for browser debugging.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+//import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from './reducers/reducers';
 import AppContainer from './components/AppContainer';
 
-const store = createStore(rootReducer);
 
-console.log(store.getState());
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(rootReducer, enhancers);
+
+//Enable hot reload when you edit a reducer (components already work)
+// if (module.hot) {
+// 	module.hot.accept('./reducers/', () => {
+// 		const nextRootReducer = require('./reducers/reducers').default;
+// 		store.replaceReducer(nextRootReducer);
+// 	})
+// }
 
 /* Test actions/reducers below: */
 // const unsubscribe = store.subscribe(() =>
